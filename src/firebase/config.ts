8200f406a01f8259bdbc77da2054af3914d107c5
Yuva Raji <rygen.dev2025@gmail.com>
2026-04-15 11:42:39 +0000

    src/firebase/config.ts (15:11) @ getFirebaseConfig  export function getF

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,17 @@
 import {type FirebaseOptions} from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBfXuVzsWrSf3Qrae6gufiAGoXFGJ7xvNY",
+  authDomain: "mobileonly-a2ef0.firebaseapp.com",
+  projectId: "mobileonly-a2ef0",
+  storageBucket: "mobileonly-a2ef0.appspot.com",
+  messagingSenderId: "1087863751665",
+  appId: "1:1087863751665:web:1428f97f0dc34668987a50"
 };
 
 export function getFirebaseConfig() {
   if (!firebaseConfig.apiKey) {
-    throw new Error('Missing NEXT_PUBLIC_FIREBASE_API_KEY. Please check your .env.local file.');
+    throw new Error('Firebase configuration is missing. Please check src/firebase/config.ts');
   }
   return firebaseConfig;
 }
