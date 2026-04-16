0bf50608a2f972dfe699b0dd61732b3569a3fd83
Yuva Raji <rygen.dev2025@gmail.com>
2026-04-16 05:04:58 +0000

    Error occurred prerendering page "/messages". Read more: https://nextjs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import * as React from "react";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React from "react";
+import * as React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
